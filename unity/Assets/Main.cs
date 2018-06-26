a4d1948a55a25c6bfc5daeb516972a38ab71e15a
Arnas Janulionis <arnas.janulionis@gmail.com>
2018-06-26 18:26:40 +0300

    added tlplib

---
+++ unity/Assets/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+	#region Unity Serialized Fields
+
+#pragma warning disable 649
+// ReSharper disable NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
+	[SerializeField, NotNull] Image image;
+
+// ReSharper restore NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
+#pragma warning restore 649
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
